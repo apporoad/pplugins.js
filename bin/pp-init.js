c8dc811f860905f02250e103f30bb0182fc5cabf
apporoad <apporoadina@163.com>
2018-12-27 13:26:40 +0800

    add1%

--- bin/pp-init.js
+++ bin/pp-init.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
+const path = require('path')
+const program = require('commander')
+const fs = require('fs')
 
-console.log("hello xxx")
+program.parse(process.argv)
+
+// console.log(program.args)
+
+var pre =()=>{
+    var initPath = program.args.length >0 ? program.args[0] : '.'
+    initPath =path.resolve(process.cwd(),initPath)
+    // console.log(initPath)
+    // when a file :  d:/yourP.js
+    
+    // when is a dir  then find all js
+}
+
+
+pre()
