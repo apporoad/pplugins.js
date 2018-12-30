2ab4134ed53f40d3078b0176e8597559b0377978
apporoad <apporoadina@163.com>
2018-12-30 20:59:29 +0800

    addppinit

--- bin/pp-init.js
+++ bin/pp-init.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const cli = require('../cli')
 const uicli = require('uicli.js')
 const find = require('find')
+const util = require('../util')
 
 program.parse(process.argv)
 
@@ -60,28 +61,127 @@ var start =()=>{
             })
         }
     }).then(plust=>{
+        // here to get some param ,need user's input
         return uicli.uiGetJson(plust)
     }).then(pluginJson=>{
-        console.log(pluginJson)
+        //console.log(pluginJson)
+        //here to save to ./plugin.json
+        SaveToPluginJSON(pluginJson)
     }).catch(reason=>{
         if(reason)
             console.log(reason)
     })
-    
+}
 
-    //uicli.uiGetJson()
+/*
+{
+    "yourModuleName" : [
+        {
+            "type" : "pluginType",
+            "version": "1.0.0",
+            "path" : "yourPlugin/p.js"
+        }
+    ]
+}
+*/
+var SaveToPluginJSON = pluginJson =>{
     /*
-    {
-        "yourModuleName" : [
-            {
-                "type" : "pluginType",
-                "version": "1.0.0",
-                "path" : "yourPlugin/p.js"
-            }
-        ]
+    { moduleName: 'pp-howtouse',
+  type: 'default',
+  rpath: 'bin/pp-howtouse.js',
+  path: '/home/rue/wp/pplugins.js/bin/pp-howtouse.js' }*/
+   var pluginJsonPath = path.join(process.cwd(),'plugin.json')
+    if(!fs.existsSync(pluginJsonPath)){
+            var trueJson = {}
+            trueJson[pluginJson.moduleName] = [{
+                type : pluginJson.type,
+                rpath : pluginJson.rpath,
+                path : pluginJson.path,
+                version : '1.0.0'
+            }]
+            writeTruJson(pluginJsonPath,trueJson)
+            return
     }
-    */
+    // here to add
+    var trueJson = require(pluginJsonPath)
+    if(!trueJson[pluginJson.moduleName] || trueJson[pluginJson.moduleName].length == 0){
+        trueJson[pluginJson.moduleName] = [{
+            type : pluginJson.type,
+            rpath : pluginJson.rpath,
+            path : pluginJson.path,
+            version : '1.0.0'
+        }]
+        writeTruJson(pluginJsonPath,trueJson)
+        return
+    }
+    var index = util.ArrayIndexOf(trueJson[pluginJson.moduleName] , pluginJson,(one,two)=>{
+        if(one.type == two.type){
+            return true
+        }
+        return false
+    })
+    if(index == -1){
+        // insert 0 
+        trueJson[pluginJson.moduleName].unshift({
+            type : pluginJson.type,
+            rpath : pluginJson.rpath,
+            path : pluginJson.path,
+            version : '1.0.0'
+        })
+        writeTruJson(pluginJsonPath,trueJson)
+        return
+    }
+    if(trueJson[pluginJson.moduleName][index].path == pluginJson.path){
+        console.log('your js is already initialed ...')
+        return
+    }
+    var minVersion = trueJson[pluginJson.moduleName][index].version
+    var splitArray = minVersion.split('.')
+    var last = parseInt(splitArray.pop()) + 1
+    var suggestVersion = splitArray.join('.') + '.' + last
+    var lustVersion={
+        version: {
+            isLust : true,
+            remark : "pliz input new version, greater than " + minVersion,
+            type : "s",
+            default : suggestVersion,
+            check : (lustInfo,data,type)=>{
+                if(!/^\d+\.\d+\.\d+$/.test(data)){
+                    console.log('your input should like x.x.x')
+                    return {}
+                }
+                if(data<= minVersion){
+                    console.log('your version must greater than ' + minVersion)
+                    return {}
+                }
+                return {isPass : true ,isUpdate : true , message : "" }
+            }
 
+        }
+    }
+    uicli.uiGetJson(lustVersion).then(vInfo =>{
+        trueJson[pluginJson.moduleName].unshift({
+            type : pluginJson.type,
+            rpath : pluginJson.rpath,
+            path : pluginJson.path,
+            version : vInfo.version
+        })
+        writeTruJson(pluginJsonPath,trueJson)
+    })
+}
+
+var writeTruJson = (apath,json) =>{
+    //sort by version first
+    for(key in json){
+        var array = json[key]
+        array.sort((a,b)=>{
+            return a.version>b.version
+        })
+        json[key] = array
+    }
+    fs.writeFileSync(apath,JSON.stringify(json),'utf8')
+    console.log("init success , " + apath )
+    console.log('...')
 }
 
 
